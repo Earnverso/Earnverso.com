3d025d9519162b3d31d952e7f295e31eed9a933c
Earnverso <dchandra727@gmail.com>
2025-05-28 16:55:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen text-2xl">
+      🚀 Welcome to Earnverso.com – Learn. Earn. Grow.
+    </div>
+  );
+}
